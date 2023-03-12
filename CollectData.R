a94d9b75abd639cb9f06885a48cc0f87edae70de
karolinenielsen <karro.nielsen@gmail.com>
2023-03-12 10:28:43 +0000

    "first commit"

---
+++ CollectData.R
@@ -0,0 +1,10 @@
+
+
+# read csv files and store in data frame
+StatueValue <- read.csv("CT5163_Project_2022_23_StatueValue_final.csv")
+TrainStation <- read.csv("CT5163_Project_2022_23_TrainStation_final.csv")
+StockPrice <- read.csv("CT5163_Project_2022_23_StockPrice_final.csv")
+
+head(StatueValue)
+head(TrainStation)
+head(StockPrice)
